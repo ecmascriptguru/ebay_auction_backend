8da17ee39ef495976ce37d533f4a4b121eb0fea3
ecmascriptguru <ecmascript.guru@gmail.com>
2017-05-09 00:34:11 -0400

    view change

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -35,7 +35,6 @@ class HomeController extends Controller {
 	 */
 	public function index()
 	{
-		
 		return view('home');
 	}
 
--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -8,7 +8,7 @@
 				<div class="panel-heading">Home</div>
 
 				<div class="panel-body">
-					You are logged in!
+					Yay, You are logged in!
 				</div>
 			</div>
 		</div>
