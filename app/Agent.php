1296a945e11f0998cdc9a1074ae04d9f293cd0a1
ecmascriptguru <ecmascript.guru@gmail.com>
2017-05-23 20:57:04 -0400

    Agent model created with file

---
+++ app/Agent.php
@@ -0,0 +1,17 @@
+<?php namespace App;
+
+use Illuminate\Database\Eloquent\Model;
+
+class Agent extends Model {
+
+	protected $fillable = ['name', 'address', 'phone'];
+	//
+	public function founder()
+    {
+        return $this->belongsTo('App\User', 'found_by');
+    }
+
+	// public function properties() {
+	// 	return $this->hasMany('App\Property');
+	// }
+}
