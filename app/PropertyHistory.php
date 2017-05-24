f561389328321ad43259a783c35a705cd1f6e9f8
ecmascriptguru <ecmascript.guru@gmail.com>
2017-05-23 21:23:12 -0400

    property history model updated

--- app/PropertyHistory.php
+++ app/PropertyHistory.php
@@ -4,6 +4,16 @@ use Illuminate\Database\Eloquent\Model;
 
 class PropertyHistory extends Model {
 
+	protected $fillable = ['price', 'currency', 'title', 'address/subtitle', 'features', 'description', 'status'];
 	//
+	public function founder()
+    {
+        return $this->belongsTo('App\User', 'created_by');
+    }
+
+	public function agent()
+    {
+        return $this->belongsTo('App\Agent', 'agent_id');
+    }
 
 }
